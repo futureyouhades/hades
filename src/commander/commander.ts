4b32a03d345b7d7073e793f45a628cfb37bc74e0
FutureYou <contact@futureyou.shop>
2026-07-03 06:34:37 +0000

    Add Commander status method

--- src/commander/commander.ts
+++ src/commander/commander.ts
@@ -1,17 +1,20 @@
 export class HadesCommander {
 
-  constructor() {}
+    constructor() {}
 
-  public start(): void {
-    console.log("Hades Commander started.");
-  }
+    public start(): void {
+        console.log("Hades Commander started.");
+    }
 
-  public stop(): void {
-    console.log("Hades Commander stopped.");
-  }
+    public stop(): void {
+        console.log("Hades Commander stopped.");
+    }
 
-  public execute(command: string): void {
-    console.log(`Received command: ${command}`);
-  }
+    public execute(command: string): void {
+        console.log(`Received command: ${command}`);
+    }
 
+    public status(): void {
+        console.log("Commander status: READY");
+    }
 }
